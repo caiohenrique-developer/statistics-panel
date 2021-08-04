d9945f576c3e177d5fc8b8bbf16edc172843abe4
caiohenrique-developer <caiorussellrox@msn.com>
2021-08-04 14:53:05 -0300

    :art: [ STYLE ]: Add company banner to company page

--- src/pages/company/index.tsx
+++ src/pages/company/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Head from 'next/head';
+import Image from 'next/image';
 
 import { Header } from '@components/Header';
 import { Menu } from '@components/Menu';
@@ -22,7 +23,20 @@ export default function Company(): JSX.Element {
         <section>
           <Header pageTitle='Empresa e Unidades' />
 
-          <main className='content'>CONTENT</main>
+          <main className='content'>
+            <div>
+              <div className='banner'>
+                <Image
+                  src='/images/main-banner-2-1030x290.jpg'
+                  width={1200}
+                  height={200}
+                />
+                <h2>Empresa</h2>
+              </div>
+
+              <h2>Unidades</h2>
+            </div>
+          </main>
         </section>
       </main>
     </Container>
--- src/styles/company-page.ts
+++ src/styles/company-page.ts
@@ -1,3 +1,35 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  .content > div {
+    margin-right: 0 !important;
+
+    .banner {
+      position: relative;
+
+      > div {
+        filter: brightness(0.5) opacity(0.2);
+      }
+
+      h2 {
+        position: absolute;
+        top: 50%;
+        right: 3.125rem;
+        bottom: 50%;
+        transform: translateY(-50%);
+
+        font-size: 1.75rem;
+      }
+    }
+
+    h2 {
+      font: var(--PoppinsBold);
+      color: var(--ice_CAC);
+    }
+
+    > h2 {
+      margin: 1.25rem 0;
+      font-size: 1.25rem;
+    }
+  }
+`;
